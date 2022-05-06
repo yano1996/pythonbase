2a5ac6dba15dfe92fb876e0899a7cf3cfff8bdfe
Marcos Yano <marcosyano@live.com.pt>
2022-05-06 20:28:14 +0900

    Hello world em varias linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-#Este programa imprime hello World
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+   export lang=pt_BR
+
+Execução:
+
+   python3 hello.py
+   ou
+   ./hello.py
+
+"""
+__version__ = "0.0.1"
+__author__ = "Marcos Yano"
+__lincense__ = "Unlincense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
+
+print(msg)
